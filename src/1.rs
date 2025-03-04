7563d43db57049b4b7e9a6492a28835442949272
takoshi6324 <ta.koshi@gmx.com>
2025-03-04 15:01:14 +0000

    Add 1.rs

---
+++ src/1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 1;
+    let y = 2;
+    let z = 3;
+
+    if x == y && y == z {
+        println!("x, y, and z are equal");
+    } else {
+        println!("x, y, and z are not equal");
+    }
+}
